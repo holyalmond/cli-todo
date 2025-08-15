5bcccf03b4944140c03b0878df8f6ed5b66127a8
holyalmond <almondawwawa@gmail.com>
2025-08-15 13:24:28 +0300

    feat: persist tasks to file between CLI runs

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 const (
-	AddTaskOption = "1"
-	ListTasksOption = "2"
+	AddTaskOption    = "1"
+	ListTasksOption  = "2"
 	ToggleTaskOption = "3"
 	DeleteTaskOption = "4"
-	ExitOption = "5"
+	ExitOption       = "5"
+	taskFile         = "tasks.json"
 )
 
 type Task struct {
@@ -24,14 +26,14 @@ type Task struct {
 func (t Task) Status() string {
 	if t.Done {
 		return "Done"
-	} 
+	}
 	return "Not done"
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	var tasks []Task
+	tasks := loadTasks()
 
 	for {
 		clearScreen()
@@ -61,7 +63,7 @@ func main() {
 	}
 }
 
-func readInput(reader *bufio.Reader) string{
+func readInput(reader *bufio.Reader) string {
 	line, _ := reader.ReadString('\n')
 	return strings.TrimSpace(line)
 }
@@ -80,6 +82,7 @@ func addTask(reader *bufio.Reader, tasks *[]Task) {
 	taskName := readInput(reader)
 	task := Task{Name: taskName, Done: false}
 	*tasks = append(*tasks, task)
+	saveTasks(*tasks)
 	fmt.Printf("Task added: %s\n", taskName)
 }
 
@@ -103,10 +106,10 @@ func selectTask(reader *bufio.Reader, tasks []Task, action string) (int, bool) {
 	listTasks(tasks)
 	fmt.Println()
 	fmt.Printf("Enter task number to %s: ", action)
-	taskNumStr:= readInput(reader)
+	taskNumStr := readInput(reader)
 
 	taskIndex, err := strconv.Atoi(taskNumStr)
-	if err != nil  || taskIndex < 1 || taskIndex > len(tasks){
+	if err != nil || taskIndex < 1 || taskIndex > len(tasks) {
 		fmt.Println("Invalid number")
 		return 0, false
 	}
@@ -115,12 +118,13 @@ func selectTask(reader *bufio.Reader, tasks []Task, action string) (int, bool) {
 }
 
 func toggleTask(reader *bufio.Reader, tasks *[]Task) {
-	taskIndex, ok  := selectTask(reader, *tasks, "toggle")
+	taskIndex, ok := selectTask(reader, *tasks, "toggle")
 	if !ok {
 		return
 	}
 
 	(*tasks)[taskIndex].Done = !(*tasks)[taskIndex].Done
+	saveTasks(*tasks)
 	fmt.Printf("%d) %s [%s]\n", taskIndex+1, (*tasks)[taskIndex].Name, (*tasks)[taskIndex].Status())
 }
 
@@ -133,9 +137,41 @@ func deleteTask(reader *bufio.Reader, tasks *[]Task) {
 	taskName := (*tasks)[taskIndex].Name
 
 	*tasks = append((*tasks)[:taskIndex], (*tasks)[taskIndex+1:]...)
+	saveTasks(*tasks)
 	fmt.Printf("Task \"%s\" deleted\n", taskName)
 }
 
 func clearScreen() {
 	fmt.Print("\033[H\033[2J")
+}
+
+func saveTasks(tasks []Task) {
+	file, err := os.Create(taskFile)
+	if err != nil {
+		fmt.Println("Error saving tasks: ", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(tasks); err != nil {
+		fmt.Println("Error encoding tasks:", err)
+	}
+}
+
+func loadTasks() []Task {
+	file, err := os.Open(taskFile)
+	if err != nil {
+		return []Task{}
+	}
+	defer file.Close()
+
+	var tasks []Task
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&tasks); err != nil {
+		fmt.Println("Error decoding tasks:", err)
+		return []Task{}
+	}
+	return tasks
 }
